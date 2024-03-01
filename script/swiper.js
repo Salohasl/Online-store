5c00cbf4e392792cf602191750299e3f89daf6f4
Maxim Zavidiy <zavidiy1998@mail.ru>
2024-03-01 12:32:37 +0200

    first commit

---
+++ script/swiper.js
@@ -0,0 +1,8 @@
+
+    var swiper = new Swiper(".mySwiper", {
+      rewind: true,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
---
+++ script/up.js
@@ -0,0 +1,13 @@
+/* Для всплывающего окна */ 
+let menuBtn = document.querySelector('.menuBtn');
+let menu = document.querySelector('nav');
+let link = document.querySelector('.link');
+menuBtn.addEventListener('click', () =>{
+menuBtn.classList.toggle('active');
+menu.classList.toggle('popUp');
+link.classList.remove('active');
+});
+
+function popupText(){
+    link.classList.toggle('active');
+}
