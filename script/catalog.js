152317fd07fd61595799dbf49bcab64dc2cf539a
Maxim Zavidiy <zavidiy1998@mail.ru>
2024-03-11 20:09:39 +0200

    dev catalog

--- script/catalog.js
+++ script/catalog.js
@@ -1,10 +1,55 @@
+import catalogAston from './arrCatalog.js';
+/*Выбор данных из массива объектов*/
+
+//В переменной хранится каталог
+let productAston  = catalogAston;
+
+//console.log(productAston);
+const product = document.querySelector('.product');
+
+//Функция выгрузки товара
+function productUnloading(){
+    for(let key of productAston){
+
+        let productCatal = document.createElement('div');
+        productCatal.classList.add('product-catal');
+        productCatal.classList.add('list');
+        product.append(productCatal);
+        
+        let img = document.createElement('img');
+        img.src = key.src;
+        productCatal.append(img);
+
+        let category = document.createElement('h4');
+        category.textContent = key.name
+        productCatal.append(category);
+
+        //создание div для центрирования описания и цены
+        let productPrice = document.createElement('div');
+        productPrice.classList.add('product-price');
+        productCatal.append(productPrice);
+
+        //описание
+        let description = document.createElement('p');
+        description.textContent = key.description;
+        productPrice.append(description);
+        //Цена
+        let price = document.createElement('p');
+        price.textContent = key.price;
+        productPrice.append(price);
+    }
+}
+productUnloading();
+
+//Функция пагинация 
+function pagination(){
 const paginationNumbers = document.getElementById("pagination-numbers");
-  const paginatedList = document.getElementById("paginated-list");
-  const listItems = paginatedList.querySelectorAll(".list");
-  const nextButton = document.getElementById("next-button");
-  const prevButton = document.getElementById("prev-button");
+const paginatedList = document.getElementById("paginated-list");
+const listItems = paginatedList.querySelectorAll(".list");
+const nextButton = document.getElementById("next-button");
+const prevButton = document.getElementById("prev-button");
   
-  const paginationLimit = 2;
+  const paginationLimit = 15;
   const pageCount = Math.ceil(listItems.length / paginationLimit);
   let currentPage = 1;
   
@@ -97,4 +142,5 @@ const paginationNumbers = document.getElementById("pagination-numbers");
       }
     });
   });
-  
+}
+pagination();
